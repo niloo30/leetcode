79c269f019a96f706fb11cd2c3a5d2304d6653b1
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-02-12 23:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (20.09%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,8 +2,13 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
 
-
-        return n >0 && ((n & (n-1))==0);
+        int ans=0;
+        while(n>0)
+        {
+            ans++;
+            n= n&(n-1);
+        }
+        return ans==1;
         
     }
 };
