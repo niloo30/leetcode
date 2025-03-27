9780c8c7a9b786df217fce3a3232ad44db8cf82e
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-03-27 19:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (79.05%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+
+        int n=arr.size();
+        int l=0;
+        int h=n-1;
+        int idx=0;
+
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+
+            if(arr[mid]>arr[mid+1])
+            {
+                idx=mid;
+                h=mid-1;
+            }
+            else
+            l=mid+1;
+        }
+
+        return idx;
+
+
+        
+    }
+};
