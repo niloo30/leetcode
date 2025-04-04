cc706dfae8167be89f8d82cb6c2f0592f6a42b2c
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-04-04 19:29:58 +0530

    Time: 3 ms (33.18%) | Memory: 10 MB (45.90%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    set<vector<int>> st;
+    void f(int i,int n,vector<int>& arr,vector<int>& v)
+    {
+        if(i==n)
+        {
+            st.insert(v);
+            return ;
+        }
+
+        v.push_back(arr[i]);
+        f(i+1,n,arr,v);
+        v.pop_back();
+        f(i+1,n,arr,v);
+
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int> v;
+        f(0,n,nums,v);
+        vector<vector<int>> ans(st.begin(),st.end());
+
+        return ans;
+        
+    }
+};
