2bf9f0047de4fb1e60cde277c1320169d2397e58
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-04-06 00:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (71.99%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans=0;
+    void f(vector<int>& nums,int i,int val,int n)
+    {
+        if(i==n)
+        {
+            ans+=val;
+            return ;
+        }
+
+        f(nums,i+1,val,n);
+        val=val^nums[i];
+        f(nums,i+1,val,n);
+    }
+    int subsetXORSum(vector<int>& nums) {
+        
+
+        int n=nums.size();
+
+        f(nums,0,0,n);
+        return ans;
+        
+    }
+};
