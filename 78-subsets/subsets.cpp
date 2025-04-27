294324ecd520f0f8845047ab2ac460ed9dd90d0a
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-04-27 23:40:15 +0530

    Time: 1 ms (48.11%) | Memory: 9.9 MB (62.19%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -11,7 +11,7 @@ public:
             vector<int> temp;
             for(int j=0;j<n;j++)
             {
-                if(i>>j &1)
+                if(i&1<<j)
                 temp.push_back(nums[j]);
             }
             ans.push_back(temp);
