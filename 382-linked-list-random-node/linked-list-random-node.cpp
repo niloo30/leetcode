7120e4c6195199e1892cbe2463202fa3a066690f
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-07-08 10:21:30 +0530

    Time: 2 ms (70.62%) | Memory: 23.5 MB (24.59%) - LeetSync

---
+++ 382-linked-list-random-node/linked-list-random-node.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    vector<int> arr;
+    Solution(ListNode* head) {
+
+        while(head)
+        {
+            arr.push_back(head->val);
+            head=head->next;
+        }
+        
+    }
+    
+    int getRandom() {
+
+        int idx=rand()%arr.size();
+
+        return arr[idx];
+        
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
