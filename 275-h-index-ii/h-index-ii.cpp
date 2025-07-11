824762c33a8f4e54ed9d85c7d465ad49f9ee6bf0
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-07-11 18:50:57 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (46.88%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool check(int mid,vector<int>& arr)
+    {
+        int idx=lower_bound(arr.begin(),arr.end(),mid)-arr.begin();
+        int num=arr.size()-idx;
+
+        return num>=mid;
+    }
+    int hIndex(vector<int>& arr) {
+
+        int l=0;
+        int h=arr.size();
+
+        int ans;
+
+        while(l<=h)
+        {
+            int mid=(l+(h-l)/2);
+
+            if(check(mid,arr))
+            {
+                ans=mid;
+                l=mid+1;
+            }
+            else
+            h=mid-1;
+        }
+
+        return ans;
+        
+    }
+};
