2abd1f977b3273b482877f8ba75666f13be3d07c
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-07-26 12:46:01 +0530

    Time: 15 ms (22.46%) | Memory: 31.8 MB (74.67%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& board) {
+
+        int m=board.size();
+        int n=board[0].size();
+        int ans=0;
+        vector<vector<int>> vis(m,vector<int>(n,0));
+
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==1)
+                {
+                    if(i==0 || i==m-1 || j==0 || j==n-1)
+                    {
+                        vis[i][j]=1;
+                        q.push({i,j});
+                    }
+                }
+            }
+        }
+
+        int dx[]={-1,0,1,0};
+        int dy[]={0,1,0,-1};
+
+        while(!q.empty())
+        {
+            int r=q.front().first;
+            int c=q.front().second;
+
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int new_r=r+dx[i];
+                int new_c=c+dy[i];
+
+                if(new_r>=0 && new_r<m && new_c>=0 && new_c<n && board[new_r][new_c]==1)
+                {
+                    if(!vis[new_r][new_c])
+                    {
+                        vis[new_r][new_c]=1;
+                        q.push({new_r,new_c});
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==1 && !vis[i][j])
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
