eac6e8231b8a56d83595063a0db819d48ee413f8
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-08-04 16:11:48 +0530

    Time: 26 ms (18.93%) | Memory: 190 MB (27.44%) - LeetSync

--- 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.cpp
+++ 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.cpp
@@ -6,27 +6,36 @@ public:
         if(n==1 || k==1)
         return nums;
         vector<int> ans;
+        deque<int> q;
 
-        int i=0,j=1;
-        int count=1;
+        int i=0,j=0;
+
         while(j<n)
         {
-            if(nums[j]==nums[j-1]+1)
-            count++;
-            else
-            count=1;
-
+            while(q.size() && nums[j]!=1+nums[q.back()])
+            {
+                q.pop_back();
+            }
+            q.push_back(j);
             if(j-i+1==k)
             {
-                if(count>=k)
-                ans.push_back(nums[j]);
+                if(q.size()>=k)
+                ans.push_back(nums[q.back()]);
                 else
                 ans.push_back(-1);
+
+                // time to slide the window 
+                if(i==q.front())
+                {
+                    q.pop_front();
+                }
                 i++;
             }
             j++;
         }
+
         return ans;
+        
         
     }
 };
