614057d3d404e084dce5f2243c026610334910ef
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-08-04 15:56:36 +0530

    Time: 4 ms (86.44%) | Memory: 184.7 MB (49.84%) - LeetSync

---
+++ 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+
+        int n=nums.size();
+        if(n==1 || k==1)
+        return nums;
+        vector<int> ans;
+
+        int i=0,j=1;
+        int count=1;
+        while(j<n)
+        {
+            if(nums[j]==nums[j-1]+1)
+            count++;
+            else
+            count=1;
+
+            if(j-i+1==k)
+            {
+                if(count>=k)
+                ans.push_back(nums[j]);
+                else
+                ans.push_back(-1);
+                i++;
+            }
+            j++;
+        }
+        return ans;
+        
+    }
+};
