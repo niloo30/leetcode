aba646f1a351d36cf6f54dfee9d7c6125785d6b8
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-08-05 23:56:05 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (26.26%) - LeetSync

---
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+
+
+        //increment n-1 ele by 1 --> decrement 1 ele by 1
+
+        // since i can only decrement i should make all the values equal to  min ele
+        
+        
+        int val=*min_element(begin(nums),end(nums));
+
+        int ans=0;
+
+        for(int ele:nums)
+        {
+            ans+=(abs(ele-val));
+        }
+        return ans;
+        
+    }
+};
