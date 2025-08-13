f033361c5ff0ce59cb446cd4c2db3d1de9149684
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-08-13 17:40:35 +0530

    Time: 4 ms (47.44%) | Memory: 9 MB (14.61%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+
+        while(n)
+        {
+            if(n==1)
+            return true;
+            if(n%3==0)
+              n=n/3;
+            else
+            return false;  
+        }
+        return false;
+        
+    }
+};
