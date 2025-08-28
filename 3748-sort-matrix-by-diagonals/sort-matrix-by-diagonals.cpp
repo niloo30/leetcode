47208045b1c430009028d007a0f60e3f58d3a66c
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-08-28 21:05:08 +0530

    Time: 23 ms (5.76%) | Memory: 46.6 MB (5.56%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+
+        int n=grid.size();
+        int m=grid[0].size();
+
+        map<int,vector<int>> mp;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            mp[i-j].push_back(grid[i][j]);
+        }
+        for(auto &ele:mp)
+        {
+            if(ele.first>=0)
+            sort(ele.second.begin(),ele.second.end());
+            else
+            sort(ele.second.rbegin(),ele.second.rend());
+        }
+        vector<vector<int>> ans(n,vector<int>(m,0));
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                int val=mp[i-j].back();
+                ans[i][j]=val;
+                mp[i-j].pop_back();
+            }
+        }
+        return ans;
+        
+    }
+};
