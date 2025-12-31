3ee398f1001b02405563bfd57319c66f59e1e244
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-12-31 21:56:29 +0530

    Time: 55 ms (46.21%) | Memory: 8.1 MB (17.49%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -2,7 +2,29 @@ class Solution {
 public:
     int maximumScore(int a, int b, int c) {
         
+        priority_queue<int> pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
 
-        return min((a+b+c)/2, (a+b+c)-max({a,b,c}));
+        int ans=0;
+
+        while(pq.size()>1)
+        {
+            int val1=pq.top();
+            pq.pop();
+            int val2=pq.top();
+            pq.pop();
+
+            ans++;
+            val1--;
+            if(val1>0)
+            pq.push(val1);
+
+            val2--;
+            if(val2>0)
+            pq.push(val2);
+        }
+        return ans;
     }
 };
