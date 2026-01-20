6a3e546cc307dbe028f61faa8f6c6419cf3cc646
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-01-20 20:29:04 +0530

    Time: 3 ms (13.92%) | Memory: 13.1 MB (8.49%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>& arr)
+    bool check(int mid,vector<int> arr)
     {
         int idx=lower_bound(arr.begin(),arr.end(),mid)-arr.begin();
-        int num=arr.size()-idx;
 
-        return num>=mid;
+        return arr.size()-idx>=mid;
     }
     int hIndex(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
+
         int l=0;
         int h=arr.size();
 
-        int ans;
+        sort(arr.begin(),arr.end());
+        int ans=0;
 
         while(l<=h)
         {
-            int mid=(l+(h-l)/2);
-
+            int mid=(h+l)/2;
             if(check(mid,arr))
             {
                 ans=mid;
@@ -26,7 +25,6 @@ public:
             else
             h=mid-1;
         }
-
         return ans;
         
     }
