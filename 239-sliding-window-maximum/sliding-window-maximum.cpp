f8734245284bfc36b74cdddcb4518843a20c50ef
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-01-20 19:27:55 +0530

    Time: 391 ms (5.95%) | Memory: 216.5 MB (5.09%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
 
-        // [-7,-8,7,5,7,1,6,0]
-        //  k=4 
-        // dry run this and u will know 
-        // we must store the index not the element 
+        vector<int> ans;
+       // deque<int> q;
+        multiset<int> st;
         int n=nums.size();
 
-        deque<int> q;
         int i=0,j=0;
-        vector<int> ans;
 
-        while(j<n)
-        {
-            // jevha paryant acha candidate milel tevha paryant
-            while(!q.empty() && nums[j]>=nums[q.back()])
-            {
-                q.pop_back();
-            }
-            q.push_back(j);
+        while(j<n){
+            st.insert(nums[j]);
 
             if(j-i+1==k)
             {
-                ans.push_back(nums[q.front()]);
-                // window slide karavi lagel right??
-                // check i wala is it the front of deque
-                if(i==q.front())
-                q.pop_front();
+                int val=*st.rbegin();
+                ans.push_back(val);
+
+                //slide the window
+                auto it = st.find(nums[i]);
+                if (it != st.end()) {
+                    st.erase(it);  
+                }
                 i++;
             }
             j++;
         }
         return ans;
+        
     }
 };
