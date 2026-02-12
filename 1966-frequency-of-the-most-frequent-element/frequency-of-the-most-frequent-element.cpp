4fd07b15dc79daa1fec58fb704bcc04ab9492576
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-02-12 15:18:00 +0530

    Time: 79 ms (8.08%) | Memory: 115.1 MB (8.89%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,72 +1,61 @@
 class Solution {
 public:
-    #define ll long long
-    // check is taking n*mid almost n^2
-    //so tc n^2logn 
-    // optimise the inner loop using prefix sum 
-    bool check(int mid,int k,vector<int>& nums,vector<ll>& presum)
+    #define ll long long 
+    bool check(ll mid,ll k,vector<int>& nums,vector<ll>& presum)
     {
-        // window ssize ->k 
-
-        // now just check ki subarray of size mid exist where we can make all eemnts equal to its last element using k operations 
+        ll i=0,j=0;
         int n=nums.size();
-        int i=0,j=0;
 
         while(j<n)
         {
             if(j-i+1==mid)
             {
-                // int diff=0;
-                // for(int k=i;k<=j;k++)
-                // {
-                //     diff+=(nums[j]-nums[k]);
-                // }
-
-                // if(diff<=k)
-                // return true;
-                ll req_sum=((ll)nums[j]*(ll)mid); // 
-                ll sum_wehave=0;
+                ll sum=presum[j];
                 if(i-1>=0)
-                sum_wehave=presum[j]-presum[i-1];
-                else
-                sum_wehave=presum[j];
+                sum-=presum[i-1];
 
-                if(k+sum_wehave>=req_sum)
+                ll reqd_sum=(j-i+1)*nums[j];
+
+
+                if(reqd_sum-sum<=k)
                 return true;
-
                 i++;
             }
+
+            
             j++;
+
         }
         return false;
     }
     int maxFrequency(vector<int>& nums, int k) {
 
+
         int n=nums.size();
+
+        ll l=1;
+        ll h=n;
+        int ans=l;
         sort(nums.begin(),nums.end());
-        int l=1;
-        int h=n;
-
         vector<ll> presum(n);
-        presum[0]=nums[0];
-
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            presum[i]=presum[i-1]+nums[i];
+            presum[i]=nums[i];
+            if(i)
+            presum[i]+=presum[i-1];
         }
-
-        int ans=1;
-        while(l<=h)
-        {
-            int mid=(l+(h-l)/2);
+        while(l<=h){
+            ll mid=(h+l)/2;
 
             if(check(mid,k,nums,presum))
             {
                 ans=mid;
                 l=mid+1;
-            }else
+            }
+            else
             h=mid-1;
         }
+
         return ans;
         
     }
