132ea6b132546e305ac80ca87546ec03934cf134
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-04-05 22:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (74.78%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -3,17 +3,27 @@ public:
     string maximumOddBinaryNumber(string s) {
 
         int n=s.size();
-        
+        int ones=0;
+        for(auto ele:s)
+        if(ele=='1')
+        ones++;
 
-        sort(s.begin(),s.end());
-        char ch=s.back();
-        s.pop_back();
+        s[n-1]='1';
+        ones--;
 
-        s=ch+s;
-        reverse(s.begin(),s.end());
-
-        // reverse(ans.begin(),ans.end());
+        for(int i=0;i<n-1;i++)
+        {
+            if(ones>0)
+            {
+                s[i]='1';
+                ones--;
+            }
+            else
+            s[i]='0';
+        }
         return s;
+
+
         
     }
 };
