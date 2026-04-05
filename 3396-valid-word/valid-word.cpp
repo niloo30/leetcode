2f20d1b4b9c7bbcbddbbf9c55528a334cdeb2199
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-04-05 21:28:14 +0530

    Time: 1 ms (18.65%) | Memory: 8.7 MB (5.83%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    bool isvowel(char ch)
-    {
-        if(ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='U' || ch=='u')
-        return true;
-
-        return false;
-    }
-    bool isconso(char ch)
-    {
-        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
-        {
-            if(!isvowel(ch))
-            return true;
-        }
-        return false;
-    }
     bool isValid(string word) {
 
-        int n=word.size();
-        if(n<3)
-        return false;
-        bool vowel=false;
-        bool consonant=false;
-        for(auto ch: word)
-        {
-            if((ch>='0' && ch<='9') || ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')))
-            {
-                if(isvowel(ch))
-                {
-                    vowel=true;
-                }
-                if(isconso(ch))
-                consonant=true;
-                
-            }
-            else return false;
+        unordered_set<char> s = {
+        'a', 'e', 'i', 'o', 'u',
+        'A', 'E', 'I', 'O', 'U'
+    };
 
-        }
+    int n = word.size();
+    if (n < 3) return false;
 
-        return vowel && consonant;
+    bool df = false, uf = false, lf = false, vf = false, cf = false;
+
+    for (auto c : word) {
+        if (!isdigit(c) && !isalpha(c)) return false;
+
+        if (isdigit(c)) df = true;
+        if (islower(c)) lf = true;
+        if (isupper(c)) uf = true;
+
+        if (s.count(c)) vf = true;
+        else if (!s.count(c) && isalpha(c)) cf = true;
+    }
+
+    return ((df || uf || lf) && vf && cf);
         
     }
 };
