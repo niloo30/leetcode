d8ace97f4f85b1953f09722d8e73eac7492355d6
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-04-07 18:59:19 +0530

    Time: 6 ms (12.19%) | Memory: 28.3 MB (27.92%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //tc O(n^2)
-        //const space
-        //approach 1 
-        
+        //tc O(n)
+        //sapce same as time
+        //approach 1  use hashmap
         int n=nums.size();
+        unordered_map<int,int> mp;
         for(int ele:nums)
+        mp[ele]++;
+
+        for(auto &it :mp)
         {
-            int count=0;
-            for(int ele2:nums)
-            {
-                if(ele==ele2)
-                count++;
-            }
-            if(count>n/2)
-            return ele;
+            if(it.second>n/2)
+            return it.first;
         }
 
         return -1;
