193cb62fd9d0efa91aaa95fc8caf6de3668541e9
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-04-20 13:28:38 +0530

    Time: 317 ms (15.63%) | Memory: 187.3 MB (43.66%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.cpp
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.cpp
@@ -2,57 +2,24 @@ class Solution {
 public:
     int longestEqualSubarray(vector<int>& nums, int k) {
 
-        if(nums.size()==1)
-        return 1;
-        if(k==0)
-        {
-            int ans=1,count=1;
-            for(int i=1;i<nums.size();i++)
-            {
-                if(nums[i]==nums[i-1])
-                {
-                    count++;
-                }
-                else
-                {
-                    ans=max(ans,count);
-                    count=1;
-                }
-            }
-            ans=max(ans,count);
-            return ans;
-        }
-
-        
-        int n=nums.size();
+        map<int,int> mp;
+        int maxi=0;
         int i=0,j=0;
-        int ans=0;
-        unordered_map<int,int> mp;
-        int maxfreq=0;
-    
+        int n=nums.size();
+
         while(j<n)
-        {
+        {   
             mp[nums[j]]++;
+            maxi=max(maxi,mp[nums[j]]);
 
-            maxfreq=max(maxfreq,mp[nums[j]]);
-
-            while((j-i+1-maxfreq)>k)
+            if(j-i+1-maxi>k)
             {
                 mp[nums[i]]--;
-                // maxfreq=0;
-
-                // for(auto ele:mp)
-                // {
-                //     maxfreq=max(maxfreq,ele.second);
-                // }
                 i++;
             }
-            ans=max(ans,maxfreq);
             j++;
-
         }
-
-        return ans;
+        return maxi;
         
     }
 };
