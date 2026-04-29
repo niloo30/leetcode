0f4aef8628f4f6ea517d1f0c6e3566e8b5bc0b14
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-04-29 19:00:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (6.69%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+
+        priority_queue<int> pq;
+
+        for(int i=1;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                pq.push(i);
+                if(pq.size()>k)
+                pq.pop();
+                if(i!=(n/i))
+                {
+                    pq.push(n/i);
+                    if(pq.size()>k)
+                    pq.pop();
+                }
+                
+            }
+        }
+        if(pq.size()<k)
+        return -1;
+        return pq.top();
+        
+    }
+};
