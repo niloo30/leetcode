224b1a0520a17cb4026177e505a6755949cca888
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-06-28 23:09:48 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (78.09%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+
+        int sum1=0,sum2=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<=9)
+            sum1+=nums[i];
+            else
+            sum2+=nums[i];
+        }
+
+        if(sum1!=sum2)
+        return true;
+
+        return false;
+        
+    }
+};
