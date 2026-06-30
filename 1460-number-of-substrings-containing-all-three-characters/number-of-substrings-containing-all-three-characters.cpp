9106e70268893168fc3c6f41ce2bfc89391055f4
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-06-30 15:18:06 +0530

    Time: 43 ms (8.23%) | Memory: 20.8 MB (5.27%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -4,26 +4,27 @@ public:
 
         int n=s.size();
 
-        unordered_map<char,int> mp;
+        map<char,int > mp;
 
         int ans=0;
-        int i=0;int j=0;
 
-        while(j<n)
-        {
-            char ch=s[j];
+        int i=0,j=0;
+
+        while(j<n){
             mp[s[j]]++;
 
-            while(mp.size()>=3)
-            {
+            while(mp.size()>=3){
+                
                 ans+=(n-j);
                 mp[s[i]]--;
                 if(mp[s[i]]==0)
                 mp.erase(s[i]);
+
                 i++;
             }
             j++;
         }
+
         return ans;
         
     }
