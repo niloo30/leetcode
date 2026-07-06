90f59eafee59309d692c1a0d4f3b7184f2855b23
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-07-06 22:24:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (70.99%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    class DisjointSet {
+    
+    public:
+    vector<int> rank, parent, size;
+
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+    int makeConnected(int n, vector<vector<int>>& connections) {
+
+        //number of componants -1
+        int m=connections.size();
+
+        if(m<n-1)
+        return -1;
+
+        DisjointSet ds(n);
+        for(int i=0;i<m;i++) {
+            
+            int u=connections[i][0];
+            int v=connections[i][1];
+
+            ds.unionBySize(u,v);
+        }
+
+        int components=0;
+
+        for(int i=0;i<n;i++)
+        if(i==ds.parent[i])
+        components++;
+
+        return components-1;
+        
+    }
+};
